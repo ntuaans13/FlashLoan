679e9a61066c8af99f50899d1d45f58d227e8293
ntuaans <ntuaans13@gmail.com>
2026-01-03 10:23:58 +0700

    update

--- src/FlashLoan.sol
+++ src/FlashLoan.sol
@@ -14,12 +14,24 @@ interface IPool {
     ) external;
 }
 
+interface IUniswapV2Router {
+    function swapExactTokensForTokens(
+        uint amountIn,
+        uint amountOutMin,
+        address[] calldata path,
+        address to,
+        uint deadlin
+    ) external returns (uint[] memory amounts);
+}
+
 contract FlashLoanBot {
     address payable public owner;
     IPool public POOL;
 
     // Dia chi Aave V3 Pool tren Ethereum Mainnet
-    address constant AAVE_POOL = 0x87870Bca3F3fD6335C3F4ce8392D69350B4fA4E2; 
+    address constant AAVE_POOL = 0x87870Bca3F3fD6335C3F4ce8392D69350B4fA4E2;
+    address constant UNISWAP_ROUTER = 0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D;
+    address constant WETH = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
 
     constructor() {
         owner = payable(msg.sender);
@@ -27,10 +39,7 @@ contract FlashLoanBot {
     }
 
     receive() external payable {}
-    
-    function _deposit() public {
-        
-    }
+
     function withdrawToken(address _token) external {
         require(msg.sender == owner, "Only owner");
         uint256 balance = IERC20(_token).balanceOf(address(this));
@@ -56,8 +65,43 @@ contract FlashLoanBot {
     ) external returns (bool) {
         uint256 myBalance = IERC20(asset).balanceOf(address(this));
         require(myBalance >= amount, "Tien chua ve!");
+        console.log("So du sau khi vay:", myBalance/1e6, "USDC");
 
         // logic kiem tien
+        address[] memory path = new address[](2); // USDC -> WETH
+        path[0] = asset;
+        path[1] = WETH;
+        IERC20(asset).approve(UNISWAP_ROUTER, amount);
+        IUniswapV2Router(UNISWAP_ROUTER).swapExactTokensForTokens(
+            amount,
+            0,
+            path,
+            address(this),
+            block.timestamp
+        );
+        myBalance = IERC20(asset).balanceOf(address(this));
+        console.log("So du sau khi trade:", myBalance/1e6, "USDC");
+
+        uint wethBalance = IERC20(WETH).balanceOf(address(this));
+        console.log("Luong WETH trade duoc", wethBalance/1e18, "WETH");
+
+        address[] memory pathBack = new address[](2); // WETH -> USDC
+        pathBack[0] = WETH;
+        pathBack[1] = asset;
+        IERC20(WETH).approve(UNISWAP_ROUTER, wethBalance);
+        IUniswapV2Router(UNISWAP_ROUTER).swapExactTokensForTokens(
+            wethBalance,
+            0,
+            pathBack,
+            address(this),
+            block.timestamp
+        );
+
+        myBalance = IERC20(asset).balanceOf(address(this));
+        console.log("So du sau khi trade back:", myBalance/1e6, "USDC");
+
+        wethBalance = IERC20(WETH).balanceOf(address(this));
+        console.log("Luong WETH trade back", wethBalance/1e18, "WETH");
 
         uint256 amountOwed = amount + premium;
         IERC20(asset).approve(address(POOL), amountOwed);
--- test/FlashLoan.t.sol
+++ test/FlashLoan.t.sol
@@ -3,15 +3,8 @@ pragma solidity ^0.8.20;
 
 import {Test} from "forge-std/Test.sol";
 import {FlashLoanBot} from "../src/FlashLoan.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "forge-std/console.sol";
-
-
-interface IERC20 {
-    function balanceOf(address account) external view returns (uint256);
-    function transfer(address to, uint256 amount) external returns (bool);
-    function approve(address spender, uint256 amount) external returns (bool);
-    function transferFrom(address from, address to, uint256 amount) external returns (bool);
-}
 
 contract FlashLoanTest is Test {
     FlashLoanBot bot;
@@ -25,16 +18,14 @@ contract FlashLoanTest is Test {
     }
 
     function testExecuteFlashLoan() public {
-        uint256 fee = 5000 * 1e6;
+        uint256 fee = 10000 * 1e6;
         deal(USDC, address(bot), fee);
         
         uint256 balanceBefore = IERC20(USDC).balanceOf(address(bot));
-        console.log("so du truoc khi vay:", balanceBefore / 1e6, "USDC");
         
         bot.requestFlashLoan(USDC, AMOUNT_TO_BORROW);
         
         uint256 balanceAfter = IERC20(USDC).balanceOf(address(bot));
-        console.log("3. So du sau khi tra no:", balanceAfter / 1e6, "USDC");
     }
 
 }
